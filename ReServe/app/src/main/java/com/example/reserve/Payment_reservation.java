3f3570634663d6af8053c9c014ebbc3076b59125
seokjun <tjrwns8024@naver.com>
2019-09-29 15:48:52 +0900

    reverse

---
+++ ReServe/app/src/main/java/com/example/reserve/Payment_reservation.java
@@ -0,0 +1,13 @@
+package com.example.reserve;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Payment_reservation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_payment_reservation);
+    }
+}
